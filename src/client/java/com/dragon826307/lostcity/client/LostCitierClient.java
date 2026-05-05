2c05d5dd0f0096921f74617b79ce9153e0b4a560
Dragon826307 <3491497043@qq.com>
2026-05-05 21:05:59 +0800

    for minecraft 1.21.11

--- src/client/java/com/dragon826307/lostcity/client/LostCitierClient.java
+++ src/client/java/com/dragon826307/lostcity/client/LostCitierClient.java
@@ -8,11 +8,11 @@ import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 import org.lwjgl.glfw.GLFW;
 
 public class LostCitierClient implements ClientModInitializer {
-    public static final KeyMapping.Category LOSTCITY = new KeyMapping.Category(ResourceLocation.withDefaultNamespace("lostcitier.general"));
+    public static final KeyMapping.Category LOSTCITY = new KeyMapping.Category(Identifier.withDefaultNamespace("lostcitier.general"));
     public static KeyMapping openGuiKey;
 	@Override
 	public void onInitializeClient() {
