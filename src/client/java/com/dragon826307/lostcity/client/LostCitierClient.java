fded910bc492d40fdd15c665468ee75ffde56b13
Dragon826307 <3491497043@qq.com>
2026-05-07 22:35:20 +0800

    for minecraft 1.21~1.21.5

--- src/client/java/com/dragon826307/lostcity/client/LostCitierClient.java
+++ src/client/java/com/dragon826307/lostcity/client/LostCitierClient.java
@@ -2,15 +2,24 @@ package com.dragon826307.lostcity.client;
 
 import com.dragon826307.lostcity.client.light_puzzle.LightPuzzleGui;
 import com.mojang.blaze3d.platform.InputConstants;
+import com.mojang.brigadier.arguments.BoolArgumentType;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.telemetry.events.WorldLoadEvent;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import org.lwjgl.glfw.GLFW;
 
 public class LostCitierClient implements ClientModInitializer {
+    public static boolean DEBUG = false;
+    protected Minecraft minecraft ;
+    public static final Component ModID = Component.empty().append("[").append(Component.literal("LostCitier").withStyle(ChatFormatting.BLUE,ChatFormatting.BOLD)).append("]");
     public static KeyMapping openGuiKey;
 	@Override
 	public void onInitializeClient() {
@@ -21,8 +30,28 @@ public class LostCitierClient implements ClientModInitializer {
                 client.setScreen(new LightPuzzleGui(Component.literal("LightPuzzleGui")));
             }
         });
+        //玩家跨越维度事件监听
+        ClientWorldEvents.AFTER_CLIENT_WORLD_CHANGE.register((minecraft, world) -> {
+            this.minecraft = minecraft;
+            if (!minecraft.isLocalServer()){
+                if (DEBUG){
+                    minecraft.gui.getChat().addMessage(Component.empty().append("[Debug]").append(world.dimension().location().toString()));
+//                    if (minecraft.getChatStatus()){
+                    minecraft.gui.getChat().addMessage(Component.empty().append("[Debug]").append(minecraft.getChatStatus().getMessage()));
+//                }
+            }
+        }});
+        //指令注册
+        ClientCommandRegistrationCallback.EVENT.register(((commandDispatcher, commandBuildContext) -> {
+            commandDispatcher.register(ClientCommandManager.literal("lostcitier").then(ClientCommandManager.literal("debug").then(ClientCommandManager.argument("debug", BoolArgumentType.bool()).executes(context -> {
+                DEBUG = BoolArgumentType.getBool(context, "debug");
+                minecraft.gui.getChat().addMessage(Component.literal(DEBUG ? "Debug Mode on" : "Debug Mode off"));
+                return 1;
+            }))).then(ClientCommandManager.literal("debug").executes(context -> {
+                DEBUG = !DEBUG;
+                minecraft.gui.getChat().addMessage(Component.literal(DEBUG ? "Debug Mode on" : "Debug Mode off"));
+                return 1;
+            })));
+        }));
 	}
-    public static Component modID(){
-        return Component.empty().append("[").append(Component.literal("LostCitier").withStyle(ChatFormatting.BLUE,ChatFormatting.BOLD)).append("]");
-    }
 }
