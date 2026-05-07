a3dfac48f341f34ea986c6cdfaed6504d0819516
Dragon826307 <3491497043@qq.com>
2026-05-07 22:35:20 +0800

    for minecraft 1.21.9~1.21.10

--- src/client/java/com/dragon826307/lostcity/client/LostCitierClient.java
+++ src/client/java/com/dragon826307/lostcity/client/LostCitierClient.java
@@ -1,33 +1,24 @@
 package com.dragon826307.lostcity.client;
 
 import com.dragon826307.lostcity.client.light_puzzle.LightPuzzleGui;
-import com.mojang.authlib.minecraft.client.MinecraftClient;
 import com.mojang.blaze3d.platform.InputConstants;
+import com.mojang.brigadier.arguments.BoolArgumentType;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientEntityEvents;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientWorldEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
-import net.fabricmc.fabric.mixin.client.rendering.ClientWorldMixin;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
-import net.minecraft.network.ClientboundPacketListener;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.level.ServerEntity;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.level.block.entity.vault.VaultBlockEntity;
 import org.lwjgl.glfw.GLFW;
 
-import java.util.Objects;
-
 public class LostCitierClient implements ClientModInitializer {
-    public static final Minecraft minecraft = Minecraft.getInstance();
+    public static boolean DEBUG = false;
+    protected Minecraft minecraft ;
     public static final Component ModID = Component.empty().append("[").append(Component.literal("LostCitier").withStyle(ChatFormatting.BLUE,ChatFormatting.BOLD)).append("]");
     public static final KeyMapping.Category LOSTCITY = new KeyMapping.Category(ResourceLocation.withDefaultNamespace("lostcitier.general"));
     public static KeyMapping openGuiKey;
@@ -42,9 +33,26 @@ public class LostCitierClient implements ClientModInitializer {
         });
         //玩家跨越维度事件监听
         ClientWorldEvents.AFTER_CLIENT_WORLD_CHANGE.register((minecraft, world) -> {
+            this.minecraft = minecraft;
             if (!minecraft.isLocalServer()){
-                System.out.println(minecraft.level.dimension().location());
+                if (DEBUG){
+                    minecraft.gui.getChat().addMessage(Component.empty().append("[Debug]").append(world.dimension().location().toString()));
+//                    if (minecraft.getChatStatus()){
+                    minecraft.gui.getChat().addMessage(Component.empty().append("[Debug]").append(minecraft.getChatStatus().getMessage()));
+//                }
             }
-        });
+        }});
+        //指令注册
+        ClientCommandRegistrationCallback.EVENT.register(((commandDispatcher, commandBuildContext) -> {
+            commandDispatcher.register(ClientCommandManager.literal("lostcitier").then(ClientCommandManager.literal("debug").then(ClientCommandManager.argument("debug", BoolArgumentType.bool()).executes(context -> {
+                DEBUG = BoolArgumentType.getBool(context, "debug");
+                minecraft.gui.getChat().addMessage(Component.literal(DEBUG ? "Debug Mode on" : "Debug Mode off"));
+                return 1;
+            }))).then(ClientCommandManager.literal("debug").executes(context -> {
+                DEBUG = !DEBUG;
+                minecraft.gui.getChat().addMessage(Component.literal(DEBUG ? "Debug Mode on" : "Debug Mode off"));
+                return 1;
+            })));
+        }));
 	}
 }
