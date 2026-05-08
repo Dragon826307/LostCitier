723424318b667800391bd764b16b929bea001dd7
Dragon826307 <3491497043@qq.com>
2026-05-08 18:22:01 +0800

    for minecraft 1.21.6~1.21.8

--- src/client/java/com/dragon826307/lostcity/client/LostCitierClient.java
+++ src/client/java/com/dragon826307/lostcity/client/LostCitierClient.java
@@ -4,6 +4,7 @@ import com.dragon826307.lostcity.client.light_puzzle.LightPuzzleGui;
 import com.mojang.blaze3d.platform.InputConstants;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientWorldEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.KeyMapping;
